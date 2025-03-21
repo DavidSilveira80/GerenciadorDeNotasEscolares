b79d24c0b12ccb29b2ecd125ebe1e9f100f53bcf
DavidSilveira80 <davidpscontato@gmail.com>
2025-03-21 20:05:12 -0300

    Implementado menuPrincipal.

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,10 @@
+fun menuPrincipal(){
+    println("------------------------------------")
+    println("   GERENCIADOR DE NOTAS ESCOLARES   ")
+    println()
+    println("      1 - CADASTRAR ALUNO           ")
+    println("      2 - ADICIONAR NOTA            ")
+    println("      3 - GERAR RELATÓRIO           ")
+    println("      4 - SAIR                      ")
+    println("------------------------------------")
+}
