abba7d219f4d493b015369f392b24c1c7e108e13
DavidSilveira80 <davidpscontato@gmail.com>
2025-03-21 20:29:48 -0300

    Definido estrutura de fluxo do programa.

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -5,6 +5,29 @@ fun menuPrincipal(){
     println("      1 - CADASTRAR ALUNO           ")
     println("      2 - ADICIONAR NOTA            ")
     println("      3 - GERAR RELATÓRIO           ")
-    println("      4 - SAIR                      ")
+    println("      4 - LISTAR ALUNOS             ")
+    println("      5 - SAIR                      ")
     println("------------------------------------")
+}
+
+
+fun main(){
+    var paradaDeFluxo = "C"
+    var opt = ""
+
+    while(paradaDeFluxo == "C"){
+        do{
+            menuPrincipal()
+            print("-> ")
+            opt = readln()
+        }while(opt != "1" && opt != "2" && opt != "3" && opt != "4" && opt != "5")
+
+        when(opt){
+            "1" -> println("CADASTRAR ALUNO")
+            "2" -> println("ADICIONAR NOTA")
+            "3" -> println("GERAR RELATÓRIO")
+            "4" -> println("LISTAR ALUNOS")
+            "5" -> paradaDeFluxo = "S"
+        }
+    }
 }
