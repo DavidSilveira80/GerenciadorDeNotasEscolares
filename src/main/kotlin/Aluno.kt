39759d675f76b81918ab5d20f7bc87113c33aae5
DavidSilveira80 <davidpscontato@gmail.com>
2025-03-21 15:13:09 -0300

    Adcionado métodos adicionarNota e ToString.

--- src/main/kotlin/Aluno.kt
+++ src/main/kotlin/Aluno.kt
@@ -1,11 +1,19 @@
 class Aluno(val nome: String, val matricula: String) {
     var notas = mutableListOf<Double>()
 
+    fun adicionarNota(nota: Double){
+        notas.add(nota)
+    }
+
     fun calculaMedia(): Double{
         return if(notas.isEmpty()){
             0.0
         }else {
             notas.sum() / notas.size
         }
+    }
+
+    override fun toString(): String {
+        return "Aluno(nome='$nome', matricula='$matricula', notas=$notas)"
     }
 }
